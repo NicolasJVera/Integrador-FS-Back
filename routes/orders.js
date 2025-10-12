6d6fc27b8257e6339fa1a2d556047eac62185629
NicolasJVera <nico03veraa@gmail.com>
2025-10-11 21:06:03 -0300

    Error de ruta en post en order que impedia crear las ordenes

--- routes/orders.js
+++ routes/orders.js
@@ -6,7 +6,7 @@ import hasErrors from "../middlewares/hasErrors.js";
 
 const router = Router();
 
-router.post("/", [protect, orderValidation, hasErrors], createOrder);
+router.post("/", [ orderValidation, hasErrors], createOrder);
 router.get("/", protect, getOrders);
 
 export default router;
